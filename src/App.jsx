badb39e37fe0abf659cdfc70002cce0105847238
PackingTuboul <gilad@tuboul.co.il>
2025-07-17 16:47:53 +0300

    Refactor App component to use customer and grade parameters from URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
   const queryParameters = new URLSearchParams(window.location.search);
 
   // קריאת הערכים מה-URL. אם הם לא קיימים, נשתמש בערכי ברירת מחדל.
-  const username = queryParameters.get("username") || "משתמש אורח";
-  const score = queryParameters.get("score") || "N/A";
+  const customer = queryParameters.get("customer") || "אורח";
+  const grade = queryParameters.get("grade") || "N/A";
 
   return (
     <div className="App"> 
       <header className="App-header">; 
         <h1>פרטי לקוח</h1>
         <div className="userInfo">
-          <p><strong>שם משתמש:</strong> {username}</p>
-          <p><strong>ציון:</strong> {score}</p>
+          <p><strong>שם לקוח:</strong> {customer}</p>
+          <p><strong>ציון:</strong> {grade}</p>
         </div>
       </header>
     </div>
